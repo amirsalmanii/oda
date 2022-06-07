ef9d7680eb71aa9155c79a9a58db09ae092a13f9
amirSalmani <salmaniamir13@gmail.com>
2022-06-07 12:38:57 +0430

    inital app

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from . import secret
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m_+2zf&70iydi=9u+pmwcj-oug(hqnub+o7h1z0t)4s$1y=#&+'
+SECRET_KEY = secret.s_k
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
