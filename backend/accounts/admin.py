33976ad165d5fa12e2e3722e979b586b20756195
amirSalmani <salmaniamir13@gmail.com>
2022-06-07 17:30:48 +0430

    add model to admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User as CustomUser
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    model = CustomUser
+    list_display = ('username', 'email', 'is_staff', 'is_superuser',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'is_active')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('id',)
+
+
+admin.site.register(CustomUser, UserAdmin)
