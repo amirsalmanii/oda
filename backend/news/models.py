ce6e17429a9f18680c0f623fc0b24384868b668c
amirSalmani <salmaniamir13@gmail.com>
2022-06-08 17:48:21 +0430

    models updated

--- backend/news/models.py
+++ backend/news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class News(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
+    title = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(upload_to="news", null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-id',)
+    
+    def __str__(self):
+        return self.title
