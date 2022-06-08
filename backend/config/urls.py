a1d3e833a38b7b5a7c7f985f71d8fc7682c9bf14
amirSalmani <salmaniamir13@gmail.com>
2022-06-08 17:51:10 +0430

    add app url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('accounts.urls')),
     path('api/v1/', include('projects.urls')),
+    path('api/v1/', include('news.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
