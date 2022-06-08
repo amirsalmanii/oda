00a77747f653550d331460092e824b28b9cc8b8f
amirSalmani <salmaniamir13@gmail.com>
2022-06-08 17:40:13 +0430

    alter model

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="projects/")
-    body = models.TextField()
+    title = models.CharField(max_length=200, null=True, blank=True)
+    image = models.ImageField(upload_to="projects/", null=True, blank=True)
+    body = models.TextField(null=True, blank=True)
 
     class Meta:
         ordering = ('-id',)
