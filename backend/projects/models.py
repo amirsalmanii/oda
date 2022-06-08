e206ef12e202246b0cccaca63e42f79dd33c9e95
amirSalmani <salmaniamir13@gmail.com>
2022-06-08 17:17:30 +0430

    update

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -5,3 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to="projects/")
     body = models.TextField()
+
+    def __str__(self):
+        return self.title
