7e6d81f3a9c3fbc978f32f1a3ad8f9074cbb5335
amirSalmani <salmaniamir13@gmail.com>
2022-06-08 17:16:10 +0430

    model impelemnted

--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="projects/")
+    body = models.TextField()
